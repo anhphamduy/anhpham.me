55e54d2c34c5498d73bdc2e45c2ef5b916d2f6f9
Anh Pham <anhphamduy@outlook.com>
2021-02-19 17:18:20 +1100

    Add blog post entity to the cms

---
+++ cms/api/blog-post/controllers/blog-post-summary.js
@@ -0,0 +1,8 @@
+module.exports = {
+  findPaths: async (ctx) => {
+    const result = await strapi
+      .query("blog-post")
+      .model.fetchAll({ columns: ["id", "published_at", "name"] }); // here we wait for one column only
+    ctx.send(result);
+  },
+};
---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9e1ac57d-dfaf-4a5f-bb06-55103e9915e9'
+};
