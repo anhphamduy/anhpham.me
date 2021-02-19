f9ec0707eb7682e4f30a52a128580b3712b9e12a
Anh Pham <anhphamduy@outlook.com>
2021-02-20 00:48:50 +1100

    More stuff

--- website/pages/components/layouts/layout.tsx
+++ website/pages/components/layouts/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Container from "./container";
 import Link from "next/link";
+import { Button } from "antd";
 
 const Layout = (props) => {
   return (
@@ -16,22 +16,47 @@ const LayoutHeader = (props) => {
     <div className="container">
       <header>
         <img src="/logo.svg" height="50px" />
-        <div>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-          <Link href="/">
-            <a>Project</a>
-          </Link>
-          <Link href="/">
-            <a>About</a>
-          </Link>
-          <Link href="/">
-            <a>Contact</a>
-          </Link>
-          <Link href="/">
-            <a>Blog</a>
-          </Link>
+        <div className="right-section">
+          <div>
+            <Link href="/">
+              <a className="active">Home</a>
+            </Link>
+            <Link href="/">
+              <a>Project</a>
+            </Link>
+            <Link href="/">
+              <a>About</a>
+            </Link>
+            <Link href="/">
+              <a>Contact</a>
+            </Link>
+            <Link href="/">
+              <a>Blog</a>
+            </Link>
+          </div>
+          <div className="social-media vertical">
+            <Button
+              type="primary"
+              shape="circle"
+              icon={<i className="fa fa-stack-overflow" aria-hidden="true" />}
+              onClick={() => {}}
+              size="large"
+            />
+            <Button
+              type="primary"
+              shape="circle"
+              icon={<i className="fa fa-github" aria-hidden="true" />}
+              onClick={() => {}}
+              size="large"
+            />
+            <Button
+              type="primary"
+              shape="circle"
+              icon={<i className="fa fa-linkedin" aria-hidden="true" />}
+              onClick={() => {}}
+              size="large"
+            />
+          </div>
         </div>
       </header>
     </div>
--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,8 +1,71 @@
 import Head from "next/head";
 import createLayout from "./components/layouts/layout";
+import React, { useState, useEffect } from "react";
+import { Button, Divider } from "antd";
+import "../styles/pages/home.less";
+import Typist from "react-typist";
+import "react-typist/dist/Typist.css";
+
+const initialSentencesToType = ["I do React", "I code cool websites"];
 
 const Home = () => {
-  return <div className="">Something</div>;
+  return (
+    <div className="home">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
+      </Head>
+      <div className="container intro-section">
+        <div className="text-italic text-light">oh, hello</div>
+        <Divider />
+
+        <div>I am Anh, a software developer and creator</div>
+        <AnimatedTyping />
+        <Button type="primary" size="large">
+          Get in touch
+        </Button>
+        <BouncingChevron />
+      </div>
+    </div>
+  );
+};
+
+const BouncingChevron = () => {
+  return <i className="fa fa-arrow-down" aria-hidden="true"></i>;
+};
+
+const AnimatedTyping = () => {
+  const [doneTyping, setDoneTyping] = useState(false);
+  const [sentencesToType, setSentencesToType] = useState(
+    initialSentencesToType
+  );
+
+  const onTypingDone = () => {
+    setSentencesToType(null);
+    setTimeout(() => {
+      setSentencesToType(initialSentencesToType);
+    }, 200);
+  };
+
+  return (
+    <div className="typist-wrapper">
+      {sentencesToType && (
+        <Typist onTypingDone={onTypingDone}>
+          <Typist.Delay ms={1000} />
+          {sentencesToType.map((i) => {
+            return (
+              <span key={i.toString()}>
+                {i}
+                {<Typist.Backspace count={20} delay={1000} />}
+              </span>
+            );
+          })}
+        </Typist>
+      )}
+    </div>
+  );
 };
 
 export default createLayout(<Home />);
