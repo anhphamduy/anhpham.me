d336f80b729a9674ebdcd7cc27a35ba8b4d578c0
Anh Pham <anhphamduy@outlook.com>
2021-02-20 20:36:32 +1100

    More responsiveness for hamburger menu

--- website/pages/components/layouts/layout.tsx
+++ website/pages/components/layouts/layout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { Button } from "antd";
 
@@ -14,6 +14,21 @@ const Layout = (props) => {
 const LayoutHeader = (props) => {
   const [isHamMenuVisible, openHamMenu] = useState(false);
 
+  const resizeHandler = () => {
+    if (window.innerWidth > 767) {
+      openHamMenu(false);
+      document.querySelector("body").classList.remove("fixedScroll");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", resizeHandler);
+
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+    };
+  }, []);
+
   return (
     <div className="container">
       <header className="d-flex mt-3">
@@ -23,32 +38,69 @@ const LayoutHeader = (props) => {
         />
         <div className="right-section">
           <div
-            className={`${
+            className={`d-md-flex align-items-center menu-link-container ${
               isHamMenuVisible
                 ? "justify-content-center"
                 : "d-none justify-content-between"
-            } d-md-flex align-items-center  menu-link-container`}
+            }`}
           >
             <div
               className={`${
-                isHamMenuVisible && "container flex-column d-flex"
-              }`}
+                isHamMenuVisible ? "container flex-column" : "flex-row"
+              } d-flex`}
             >
-              <Link href="/">
-                <a className="active">Home</a>
-              </Link>
-              <Link href="/">
-                <a>Project</a>
-              </Link>
-              <Link href="/">
-                <a>About</a>
-              </Link>
-              <Link href="/">
-                <a>Contact</a>
-              </Link>
-              <Link href="/">
-                <a>Blog</a>
-              </Link>
+              <div>
+                <Link href="/">
+                  <a className="active">Home</a>
+                </Link>
+              </div>
+              <div>
+                <Link href="/">
+                  <a>Project</a>
+                </Link>
+              </div>
+              <div>
+                <Link href="/">
+                  <a>About</a>
+                </Link>
+              </div>
+              <div>
+                <Link href="/">
+                  <a>Contact</a>
+                </Link>
+              </div>
+              <div>
+                <Link href="/">
+                  <a>Blog</a>
+                </Link>
+              </div>
+
+              <div className="mt-5 d-block d-md-none">
+                <Button
+                  type="primary"
+                  shape="circle"
+                  icon={
+                    <i className="fa fa-stack-overflow" aria-hidden="true" />
+                  }
+                  onClick={() => {}}
+                  size="large"
+                />
+                <Button
+                  className="mx-4"
+                  type="primary"
+                  shape="circle"
+                  icon={<i className="fa fa-github" aria-hidden="true" />}
+                  onClick={() => {}}
+                  size="large"
+                />
+                <Button
+                  type="primary"
+                  shape="circle"
+                  icon={<i className="fa fa-linkedin" aria-hidden="true" />}
+                  onClick={() => {}}
+                  size="large"
+                />
+              </div>
             </div>
           </div>
           <div className="d-block d-md-none">
@@ -103,5 +155,36 @@ const LayoutHeader = (props) => {
 const createLayout = (children) => {
   return () => <Layout>{children}</Layout>;
 };
+
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+
+  return windowSize;
+}
 
 export default createLayout;
