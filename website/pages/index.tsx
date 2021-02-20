ba6dcfefb08cb0607bc9cb7eaf4d13563d684b38
Anh Pham <anhphamduy@outlook.com>
2021-02-21 02:19:28 +1100

    More stuff

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,12 +1,13 @@
 import Head from "next/head";
 import createLayout from "./components/layouts/layout";
 import React, { useState, useEffect } from "react";
-import { Button, Divider } from "antd";
+import { Button, Card, Col, Divider, Row, Typography } from "antd";
 import "../styles/pages/home.less";
 import Typist from "react-typist";
 import "react-typist/dist/Typist.css";
 import Title from "antd/lib/typography/Title";
 import Paragraph from "antd/lib/typography/Paragraph";
+import Meta from "antd/lib/card/Meta";
 
 const initialSentencesToType = ["I do React", "I code cool websites"];
 
@@ -19,7 +20,7 @@ const Home = () => {
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         />
       </Head>
-      <div className="container intro-section">
+      <div className="container intro-section py-5 my-5">
         <div className="text-italic text-light">oh, hello</div>
         <Divider />
 
@@ -33,10 +34,9 @@ const Home = () => {
         </div>
       </div>
       <div className="brief-summary-section">
-        <img src="/Slanted-Gradient.svg" />
-        <div className="content primary-background">
+        <div className="content">
           <div className="container">
-            <div className="text-wrapper">
+            <div className="text-wrapper py-5">
               <i className="fa fa-map-marker" aria-hidden="true"></i>
               <Title level={2}>Based in Melbourne, Australia</Title>
               <Paragraph>
@@ -52,13 +52,65 @@ const Home = () => {
                 I love travelling and I am willing to relocate anywhere for
                 work!
               </Paragraph>
-              <Button type="primary" size="large">
+              <Button className="mt-5" type="primary" size="large">
                 Learn More About Me
               </Button>
             </div>
           </div>
         </div>
-        <img src="/Slanted-Gradient.svg" className="inverted-image" />
+      </div>
+      <div className="py-5 container">
+        <Title level={1}>Publication</Title>
+        <Row gutter={[16, 16]}>
+          <Col span={8}>
+            <Card
+              hoverable
+              cover={
+                <img
+                  alt="example"
+                  src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                />
+              }
+            >
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
+              />
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card
+              hoverable
+              cover={
+                <img
+                  alt="example"
+                  src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                />
+              }
+            >
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
+              />
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card
+              hoverable
+              cover={
+                <img
+                  alt="example"
+                  src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                />
+              }
+            >
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
+              />
+            </Card>
+          </Col>
+        </Row>
       </div>
     </div>
   );
