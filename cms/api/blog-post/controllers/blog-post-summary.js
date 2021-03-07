578be07acf083e3120863fb828916b736f0e3b0f
Anh Pham <anhphamduy@outlook.com>
2021-03-08 10:49:18 +1100

    Add snippet field to blog model

--- cms/api/blog-post/controllers/blog-post-summary.js
+++ cms/api/blog-post/controllers/blog-post-summary.js
@@ -2,7 +2,7 @@ module.exports = {
   findPaths: async (ctx) => {
     const result = await strapi
       .query("blog-post")
-      .model.fetchAll({ columns: ["id", "published_at", "name"] }); // here we wait for one column only
+      .model.fetchAll({ columns: ["id", "published_at", "name", "snippet"] }); // here we wait for one column only
     ctx.send(result);
   },
 };
