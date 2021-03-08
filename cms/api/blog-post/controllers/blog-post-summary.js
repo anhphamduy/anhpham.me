cfe203251de571c4e352e57e90311af76c8dee88
Anh Pham <anhphamduy@outlook.com>
2021-03-09 00:36:22 +1100

    Add createdAt

--- cms/api/blog-post/controllers/blog-post-summary.js
+++ cms/api/blog-post/controllers/blog-post-summary.js
@@ -3,7 +3,7 @@ module.exports = {
     const result = await strapi
       .query("blog-post")
       .model.find({})
-      .select("snippet name created_at");
+      .select("snippet name createdAt");
     ctx.send(result);
   },
 };
